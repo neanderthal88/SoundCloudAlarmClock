6a99fddeaa3e69278517db7453ae51ba499e9861
rgliever <ryangliever@gmail.com>
2015-01-10 16:56:49 -0800

    Added AsyncTask for user authentication
    also reverted back to soundcloud api instead of the other java library.
    
    https://github.com/soundcloud/java-api-wrapper/

--- app/src/main/java/com/example/loginscreen/MainActivity.java
+++ app/src/main/java/com/example/loginscreen/MainActivity.java
@@ -3,13 +3,16 @@ package com.example.loginscreen;
 import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.AsyncTask;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-import de.voidplus.soundcloud.*;
+import android.util.Log;
+import com.soundcloud.api.*;
+import com.google.gson.*;
 
 public class MainActivity extends Activity {
 
@@ -27,15 +30,16 @@ public class MainActivity extends Activity {
         attempts = (TextView)findViewById(R.id.textView5);
         attempts.setText(Integer.toString(counter));
         login = (Button)findViewById(R.id.button1);
-
-        SoundCloud soundcloud = new SoundCloud(
-                "7b0837389ef02bedd507d700b962f5a2", /* appClientId */
-                "e81408f1564aab7c90af0a65d99e6d1e" /* appClientSecret */
-        );
     }
 
     public void login(View view){
-        if(username.getText().toString().equals("admin") &&
+
+        System.out.println("user: " +username.getText().toString()+ " pass: " +password.getText().toString());
+
+        authUser task = new authUser();
+        task.execute(new String[]{username.getText().toString(), password.getText().toString()});
+
+       /* if(username.getText().toString().equals("admin") &&
                 password.getText().toString().equals("admin")){
             Toast.makeText(getApplicationContext(), "Redirecting...",
                     Toast.LENGTH_SHORT).show();
@@ -49,10 +53,34 @@ public class MainActivity extends Activity {
             if(counter==0){
                 login.setEnabled(false);
             }
-
-        }
+        } */
 
     }
+
+    private class authUser extends AsyncTask<String, Void, String> {
+        protected String doInBackground(String... params) {
+            boolean debug = false;
+            try {
+                System.out.println("hello");
+                ApiWrapper soundcloud = new ApiWrapper(
+                        "7b0837389ef02bedd507d700b962f5a2",
+                        "e81408f1564aab7c90af0a65d99e6d1e",
+                        null, null
+                );
+                System.out.println("hello2");
+                Token token = soundcloud.login(
+                        params[0],
+                        params[1]
+                );
+                debug = token.valid();
+                System.out.println("debug: " +debug);
+            } catch (Exception e) {
+                System.out.println(e.toString());
+            }
+            return "return-tmp";
+        }
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
